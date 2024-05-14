f8f771899cf67d3e40e8a75d9e4a88e1b70ce168
joluj <johannes.lukas.jablonski@gmail.com>
2024-05-14 15:15:43 +0200

    finish poc flowers

--- predict.py
+++ predict.py
@@ -10,7 +10,7 @@ model = keras.models.load_model('model.keras')
 rose = './flower_photos/roses/12240303_80d87f77a3_n.jpg'
 daisy = 'flower_photos/daisy/5794839_200acd910c_n.jpg'
 
-img = image.load_img(daisy, target_size=(img_width, img_height))
+img = image.load_img(rose, target_size=(img_width, img_height))
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
 
@@ -18,3 +18,6 @@ x = np.expand_dims(x, axis=0)
 predict_x = model.predict(x)
 classes_x = np.argmax(predict_x, axis=1)
 print(classes_x)
+
+class_names = ['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips']
+print("Predicted class: " + class_names[classes_x[0]])
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Hackbay 2024
+
+## Setup Flower POC
+
+- Download and extract https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz
