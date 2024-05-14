d4fd2813779f35e04ef5824be3b7fde54581eeab
joluj <johannes.lukas.jablonski@gmail.com>
2024-05-14 15:11:20 +0200

    init poc flowers

---
+++ predict.py
@@ -0,0 +1,20 @@
+from tensorflow import keras
+from keras.preprocessing import image
+import numpy as np
+
+img_height = 180
+img_width = 180
+
+model = keras.models.load_model('model.keras')
+
+rose = './flower_photos/roses/12240303_80d87f77a3_n.jpg'
+daisy = 'flower_photos/daisy/5794839_200acd910c_n.jpg'
+
+img = image.load_img(daisy, target_size=(img_width, img_height))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+
+# images = np.vstack([x])
+predict_x = model.predict(x)
+classes_x = np.argmax(predict_x, axis=1)
+print(classes_x)
